f9a0a282bf7d22a19bc081fbc02f0758d1946a0a
hka <152134945+Sarthak-Saini07@users.noreply.github.com>
2026-02-09 11:42:29 +0530

    server starting

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+const app = express();
+dotenv.config();
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("User Management API is running");
+});
+
+// app.use("/api/users", userRouter);
+
+export default app;
